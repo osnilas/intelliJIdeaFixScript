e104838e6a00f30191be075bd8800aa8903be20d
Afonso Cunha <134631872+osnilas@users.noreply.github.com>
2024-02-28 21:13:34 +0000

    Update InteliJFixScript.sh

--- InteliJFixScript.sh
+++ InteliJFixScript.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-# Specify the directory path
 directory_path="/home/osno/.var/app/com.jetbrains.IntelliJ-IDEA-Community/config/JetBrains/IdeaIC2023.3"
 
-# Check if the directory exists
 if [ -d "$directory_path" ]; then
-    # Change to the specified directory
+
     cd "$directory_path" || exit
 
-    # Check if the file ".lock" exists
     if [ -e ".lock" ]; then
-        # Remove the file ".lock"
         rm -f ".lock"
         echo "File '.lock' removed successfully."
     else
